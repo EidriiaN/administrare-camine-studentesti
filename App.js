9e51a9d37501ad2b7604bf67dc285a29fbcce1bd
EidriiaN <adrianneagu309@gmail.com>
2023-11-26 02:16:52 +0200

    update

--- App.js
+++ App.js
@@ -1,20 +1,112 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  Alert,
+  BackHandler,
+  ActivityIndicator,
+  View,
+  Platform,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./screens/login";
+import Home from "./screens/home";
+import Test from "./screens/test";
+import axios from "axios";
+import { useLoading } from "./components/LoadingContext";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [isCheckingSession, setIsCheckingSession] = useState(true);
+  const [isLogged, setIsLogged] = useState(false);
+  const { showLoading, hideLoading } = useLoading();
+
+  useEffect(() => {
+    const handleBackButton = () => {
+      Alert.alert(
+        "Exit From App",
+        "Do you want to exit from the app?",
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel"),
+            style: "cancel",
+          },
+          { text: "OK", onPress: () => BackHandler.exitApp() },
+        ],
+        { cancelable: false }
+      );
+      return true;
+    };
+
+    if (Platform.OS === "android" || Platform.OS === "ios") {
+      BackHandler.addEventListener("hardwareBackPress", handleBackButton);
+    }
+
+    return () => {
+      if (Platform.OS === "android" || Platform.OS === "ios") {
+        BackHandler.removeEventListener("hardwareBackPress", handleBackButton);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    const checkIfIsLogged = async () => {
+      try {
+        showLoading();
+        const response = await axios.get("http://localhost:3000/checkSession", {
+          withCredentials: true,
+        });
+
+        if (response.status === 200) {
+        }
+      } catch (error) {
+        // Gestionare eroare: afișați un mesaj sau faceți altceva pentru a informa utilizatorii despre o eroare
+        console.error("Error checking session:", error);
+      } finally {
+        // Indiferent de rezultat, setați starea de verificare a sesiunii ca fiind finalizată
+        setIsCheckingSession(false);
+        hideLoading();
+      }
+    };
+
+    checkIfIsLogged();
+  }, []);
+
+  if (isCheckingSession) {
+    // Încă se verifică sesiunea, puteți afișa, de exemplu, un ecran de încărcare
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator
+          size={Platform.OS == "web" ? 80 : "large"}
+          color="#00BFFF"
+        />
+      </View>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app2!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style="inverted" />
+
+      <Stack.Navigator>
+        {isLogged ? (
+          <>
+            <Stack.Screen name="home" component={Home} />
+            <Stack.Screen name="test" component={Test} />
+          </>
+        ) : (
+          <>
+            <Stack.Screen
+              name="login"
+              component={Login}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="home" component={Home} />
+            <Stack.Screen name="test" component={Test} />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/login.js
@@ -0,0 +1,150 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import {
+  View,
+  Image,
+  Text,
+  TextInput,
+  Button,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+} from "react-native";
+const logo = require("../assets/logo-upg-2.png");
+
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const validateForm = () => {
+    let errors = {};
+
+    if (!email) errors.email = "Email is required";
+    if (!password) errors.password = "Password is required";
+
+    setErrors(errors);
+
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleSubmit = () => {
+    if (validateForm()) {
+      handleLogin();
+    }
+  };
+
+  const handleLogin = () => {
+    const credentials = {
+      email: email,
+      password: password,
+    };
+
+    axios
+      .post("http://localhost:3000/login", credentials, {
+        withCredentials: true,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          console.log("Autentificare reușită!");
+          setEmail("");
+          setPassword("");
+          setErrors({});
+          navigation.replace("home");
+          // Executați acțiuni corespunzătoare pentru autentificarea reușită
+        } else {
+          console.log("Autentificare eșuată!");
+          // Executați acțiuni corespunzătoare pentru autentificarea eșuată
+        }
+      })
+      .catch((error) => {
+        console.error("Eroare:", error);
+      });
+  };
+
+  // const loggedIn = req.session.loggedIn;
+
+  return (
+    <KeyboardAvoidingView
+      behavior="padding"
+      keyboardVerticalOffset={-160}
+      style={styles.container}
+    >
+      <View style={styles.form}>
+        <Image style={styles.logo} source={logo} resizeMode="contain" />
+        <Text style={styles.label}>Email</Text>
+        <TextInput
+          style={styles.input}
+          value={email}
+          onChangeText={setEmail}
+          placeholder="Enter your email"
+        />
+        {errors.email ? (
+          <Text style={styles.errorText}>{errors.email}</Text>
+        ) : null}
+        <Text style={styles.label}>Password</Text>
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={setPassword}
+          placeholder="Enter your password"
+          secureTextEntry
+        />
+        {errors.password ? (
+          <Text style={styles.errorText}>{errors.password}</Text>
+        ) : null}
+        <Button title="Login" onPress={handleSubmit} />
+      </View>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 15,
+    backgroundColor: "#f5f5f5",
+  },
+  logo: {
+    width: 300,
+    height: 300,
+    alignSelf: "center",
+    marginTop: 5,
+    marginBottom: 20,
+  },
+  form: {
+    width: Platform.OS === "web" ? 700 : null,
+    alignSelf: Platform.OS == "web" ? "center" : null,
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+    fontWeight: "bold",
+  },
+  input: {
+    height: 50,
+    borderColor: "#ddd",
+    borderWidth: 1,
+    marginBottom: 10,
+    padding: 10,
+    borderRadius: 7,
+  },
+  errorText: {
+    color: "red",
+    marginBottom: 10,
+  },
+});
